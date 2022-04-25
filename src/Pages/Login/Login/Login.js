1b03b5607e2741a6d7d8cb675e18e02a2797cd37
frhabibur <frhabibur45@gmail.com>
2022-04-25 12:59:59 +0600

    dynamic title done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -7,6 +7,7 @@ import Loading from '../../Shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import PageTitle from '../../Shared/PageTitle/PageTitle';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -55,13 +56,15 @@ const Login = () => {
             await sendPasswordResetEmail(email);
             toast('Sent email');
         }
-        else{
+        else {
             toast('please enter your email address');
         }
     }
 
     return (
+
         <div className='container w-50 mx-auto'>
+            <PageTitle title={'Login'}></PageTitle>
             <h2 className='text-primary text-center mt-2'>Please Login</h2>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
