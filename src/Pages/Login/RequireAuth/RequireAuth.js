ca8b466477fb17d93b4b9d779a0f4dce7e59434f
frhabibur <frhabibur45@gmail.com>
2022-04-27 10:20:00 +0600

    backup-genius-car

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
-const RequireAuth = ({children}) => {
+const RequireAuth = ({ children }) => {
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(
+        auth
+    );
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
-    if(loading){
+    if (loading) {
         return <Loading></Loading>
     }
-    if(!user){
+    if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+    else if (!user.emailVerified) {
+        return <div>
+            <h2 className='text-danger'>Your Email is not Varified </h2>
+            <h5 className='text-success'>Please Varify Your Email</h5>
+            <button
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Sent email');
+
+
+                }}
+            >
+                Verify email
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
     }
     return children;
 };
